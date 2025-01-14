88c97f0803a05f0a05469c667deb668e576a81e4
Alabs02 <alabson.inc@gmail>
2025-01-14 07:38:08 +0100

    Improved Accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -131,8 +131,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  maximumScale: 2,
+  userScalable: true,
   interactiveWidget: "resizes-visual",
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: "#06b6d4" },
